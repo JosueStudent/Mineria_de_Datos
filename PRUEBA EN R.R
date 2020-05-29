b23b7ac73566043dfa97cfe8bf16e8bedd8175b1
David7Davila <david7davila@gmail.com>
2020-05-29 18:06:13 -0500

    UP3

---
+++ PRUEBA EN R.R
@@ -0,0 +1,9 @@
+
+C:/Users/david/Mineria Datos/Josue/Mineria_de_Datos/Avance PIA/DATAVIDEOJUEGOS.csv
+
+DATAJUEGO<-read.csv("C:/Users/david/Mineria Datos/Josue/Mineria_de_Datos/Avance PIA/DATAVIDEOJUEGOS.csv",sep = ",")
+attach(DATAJUEGO)
+names(DATAJUEGO)
+#Regresion LogÃ?stica (modelo logit)
+modelo.logit <- glm(Exito ~ Plataforma+Genero+Score+ESRB, family = "binomial")
+summary(modelo.logit)
