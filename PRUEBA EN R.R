5c4814ddcf45bf377fba5e7e4e8da919e6a3adb1
David7Davila <david7davila@gmail.com>
2020-06-01 16:13:32 -0500

    PIA FINAL

--- PRUEBA EN R.R
+++ PRUEBA EN R.R
@@ -1,9 +1,15 @@
 
 C:/Users/david/Mineria Datos/Josue/Mineria_de_Datos/Avance PIA/DATAVIDEOJUEGOS.csv
 
-DATAJUEGO<-read.csv("C:/Users/david/Mineria Datos/Josue/Mineria_de_Datos/Avance PIA/DATAVIDEOJUEGOS.csv",sep = ",")
+DATAJUEGO<-read.csv("C:/Users/david/Mineria Datos/Josue/Mineria_de_Datos/Avance PIA/DATAVIDEOJUEGOSCAT.csv",sep = ",")
 attach(DATAJUEGO)
 names(DATAJUEGO)
 #Regresion LogÃ?stica (modelo logit)
-modelo.logit <- glm(Exito ~ Plataforma+Genero+Score, family = "binomial")
+
+modelo.logit <- glm(X4 ~ X0+X1+X2+X3, family = "binomial")
 summary(modelo.logit)
+anova(modelo.logit)
+modelo.logit
+
+plot(modelo.logit$fitted.values,col=c(1,2,3,4))
+plot(modelo.logit$y)
